020b066a92643fe7523c23a2682b9aae5d4438db
Logan Thomley <logancthomley@gmail.com>
2025-07-25 04:39:58 -0400

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
 
 
@@ -9,6 +10,7 @@ for year in range(1969,2025):
     temp_df = pd.read_csv(f'tennis_atp/atp_matches_{year}.csv')
     df = pd.concat([df,temp_df])
 
+print(df.head())
 # ========================= Wins by Surface =========================
 surface_wins = df.groupby(['winner_name', 'surface']).size().unstack(fill_value=0)
 
@@ -29,18 +31,18 @@ hard_stats = surface_stats_df.sort_values('hard_wins',ascending=False).iloc[:20,
 total_stats = surface_stats_df.sort_values('total_wins',ascending=False).iloc[:20,:]
 
 # Plotting each set of data
-'''
-grass_stats.plot(x='player', y='grass_wins', kind='bar',color='green',title='ATP Match Wins on Grass Courts (Open Era)')
+
+grass_stats.plot(x='player', y='grass_wins', kind='bar',color='#269281',title='ATP Match Wins on Grass Courts (Open Era)')
 plt.tight_layout()
-clay_stats.plot(x='player', y='clay_wins', kind='bar',color='orange',title='ATP Match Wins on Clay Courts (Open Era)')
+clay_stats.plot(x='player', y='clay_wins', kind='bar',color='#269281',title='ATP Match Wins on Clay Courts (Open Era)')
 plt.tight_layout()
-hard_stats.plot(x='player', y='hard_wins', kind='bar',color='blue',title='ATP Match Wins on Hard Courts (Open Era)')
+hard_stats.plot(x='player', y='hard_wins', kind='bar',color='#269281',title='ATP Match Wins on Hard Courts (Open Era)')
 plt.tight_layout()
-total_stats.plot(x='player', y='total_wins', kind='bar',color='red',title='ATP Match Wins on All Courts (Open Era)')
+total_stats.plot(x='player', y='total_wins', kind='bar',color='#269281',title='ATP Match Wins on All Courts (Open Era)')
 plt.tight_layout()
 
 plt.show()
-'''
+
 
 # ========================= Wins by Age =========================
 
@@ -55,16 +57,18 @@ youngest_winner = df.loc[df['winner_age']==youngest_win_age]['winner_name']
 
 # Boxplot for ages of ATP match winners
 sns.set_theme(style='whitegrid')
-fig1 = sns.boxplot(data=df['winner_age'],orient='h',width=.5,fliersize=2,linecolor="#000000",color="#C1024B")
-fig1.set(title='Age of ATP Match Winners',ylabel='',xlabel='')
+fig1 = sns.boxplot(data=df['winner_age'],orient='h',width=.5,fliersize=2,linecolor="#000000",color="#269281")
+fig1.set(title='Age of ATP Match Winners',ylabel='',xlabel='Years')
 plt.show()
+
+print(avg_win_age,oldest_win_age,oldest_winner,youngest_win_age,youngest_winner,sep='\n')
 
 
 # ========================= Wins by Country =========================
 
 # Ranking countries by # of ATP match wins
 country_win_stats = df.groupby(['winner_ioc']).size().sort_values(ascending=False)
-fig2 = sns.barplot(data=country_win_stats[:20],orient='h',color="#F63B11")
+fig2 = sns.barplot(data=country_win_stats[:20],orient='h',color="#269281")
 fig2.set(title='ATP Match Wins by Country',ylabel='Country',xlabel='# of Match Wins')
 plt.show()
 
@@ -76,6 +80,10 @@ other_players = players_per_country[15:].sum()
 players_per_country = players_per_country[:14]
 players_per_country.loc['Other'] = other_players
 
-fig3 = players_per_country.plot(kind='pie', title='% of Players from each Country',colormap='magma')
+
+colors = [(0.14901960784313725, 0.5725490196078431, 0.5058823529411764), (.9, .9, .9)] 
+cm = LinearSegmentedColormap.from_list(
+        "Custom", colors, N=30)
+fig3 = players_per_country.plot(kind='pie', title='% of Players from each Country',colormap=cm)
 plt.show()
 
